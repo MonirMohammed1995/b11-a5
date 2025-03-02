534dc581143acb063a5d6d74217719ab8162045c
MonirMohammed1995 <monirmdnayemsaju42@gmail.com>
2025-03-02 22:49:57 +0600

    Add Clear Button succesfully

--- js/increasingDecreasing.js
+++ js/increasingDecreasing.js
@@ -3,33 +3,34 @@ let decreaseText = document.getElementById("decreaseText");
 let title = document.querySelectorAll(".title");
 let buttons = document.querySelectorAll(".actionBtn");
 
-function myFunction() {
-    const p = document.getElementById("msgBox");
-    p.remove();
-}
-
-function addMsg(){
-let container = document.getElementById("msgBox");
-let currentTime = new Date().toLocaleTimeString();
-const p = document.createElement("p");
-p.innerText = `You have Complete The Task ${title} at ${currentTime}`;
-container.appendChild(p);
+function addMsg() {
+  let container = document.getElementById("msgBox");
+  let currentTime = new Date().toLocaleTimeString();
+  const p = document.createElement("p");
+  p.innerText = `You have Complete The Task ${title} at ${currentTime}`;
+  container.appendChild(p);
 }
 
 function checkAllDisabled() {
-    let allDisabled = Array.from(buttons).every(button => button.disabled);
-    if (allDisabled) {
-        alert("All Task are done successfully");
-    }
+  let allDisabled = Array.from(buttons).every((button) => button.disabled);
+  if (allDisabled) {
+    alert("All Task are done successfully");
+  }
 }
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    increaseText.innerText = parseInt(increaseText.innerText) + 1;
+    decreaseText.innerText = parseInt(decreaseText.innerText) - 1;
+    button.disabled = true;
+    alert("Updated Successfully");
+    checkAllDisabled();
+    addMsg();
+  });
+});
 
-buttons.forEach(button => {
-    button.addEventListener("click", function() {
-        increaseText.innerText = parseInt(increaseText.innerText) + 1;
-        decreaseText.innerText = parseInt(decreaseText.innerText) - 1;
-        button.disabled = true;
-        alert("Updated Successfully");
-        checkAllDisabled();
-        addMsg();
-    });
-})
+p = document.getElementById("msgBox");
+const removeTextBtn = document.getElementById("clearText");
+
+clearText.addEventListener("click", function () {
+  p.innerText = ""; // Remove the text
+});
